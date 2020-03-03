f705c382f11f345d23e565288fb38099af44b783
komald4 <komald.4@gmail.com>
2020-03-03 16:43:36 -0500

    Changes to Project

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,25 @@
 class AppointmentsController < ApplicationController
+
+
+    def show
+        @appointment = Appointment.find(params[:id])
+    end
+
+    def new
+        @appointment.new
+    end
+
+    def create
+        @appointment = Appointment.create(appointment_params)
+
+        redirect_to @appointment
+    end
+
+    private
+
+    def appointments_params
+        params.require.(:appointment).permit(:user_id, :pet_id)
+        
+    end
+    
 end
--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -4,6 +4,8 @@ class SheltersController < ApplicationController
      @shelters = Shelter.all 
     end
 
-
-
+    def show
+        @shelter = Shelter.find(params[:id])
+    end
+    
 end
