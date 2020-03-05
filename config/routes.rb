fd4cb429aea52948cc0ad0878e728dbab93d20d1
komald4 <komald.4@gmail.com>
2020-03-05 10:12:51 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,19 @@ Rails.application.routes.draw do
   resources :appointments
   resources :pets
   resources :shelters
-  resources :users
+
+  root: '/login'
+
+  
+
+  resources :users, only: [:new, :create, :destroy]
+  get '/profile', to: 'users#show' 
+  
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
