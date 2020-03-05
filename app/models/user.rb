30b0cf39a56cffaff06011ab5262c34d858f93e3
komald4 <komald.4@gmail.com>
2020-03-05 10:09:58 -0500

    User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ApplicationRecord
+
     has_many :appointments 
     has_many :pets, through: :appointments
 
+has_secure_password
+
+
+
+#   attr_reader :password
+
+    # def password=(plain_text_pw)
+    #      self.password_digest = BCrypt::Password.create(plain_text_pw)
+    # end
+
+    # def authenticate(pt_pw)
+    #   if BCrypt::Password.new(self.password_digest)  == pt_pw
+    #     @user
+    #   else
+    #     false
+    #   end   
+    # end
 end
