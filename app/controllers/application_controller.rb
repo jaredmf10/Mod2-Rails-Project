ccb481f14ea184adcbf3ac01bf3a4fc18ff7af08
komald4 <komald.4@gmail.com>
2020-03-05 10:09:04 -0500

    Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+before_action :authorized
+helper_method :current_user
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def authorized
+        redirect_to login_path unless logged_in?
+    end
 end
