796fa9189cd180595843b4a3960d9510fcf2a009
Leon Jamison <leonjamison@icloud.com>
2020-03-06 13:49:52 -0500

    Final Project

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link singledog.mp4
+//= link DogandParent.mp4
+//= link homepagedog.mp4
+//= link video.mp4
+//= link puppylove.mp4
